37c1bc37756902af8844f319c03e36245e58b468
Andrew26nin <tran26@mail.ru>
2016-07-08 14:26:04 +0300

    Ладноооо

--- dist/index.php
+++ dist/index.php
@@ -35,24 +35,24 @@ $(document).ready(function() {
 
   	<header class="header">
 		<img src="img/logo.png" id="logo">
-		<div id="head-txt">СОГЛАСОВАНИЕ  В ТУЛЕ</div>
+		<div id="head-txt">СОГЛАСОВАНИЕ  В КАЛУГЕ</div>
 <div id="head-phone">8 920 751-80-00<p><a class="various" href="#form-ite">Заказать обратный звонок</a></p></div>
 	</header><!-- .header-->
 
-  
+
 <main class="content">
 		<div id="content-h">ОФОРМЛЕНИЕ&nbsp;КОЛОРИСТИЧЕСКОГО&nbsp;ПАСПОРТА</div>
-			<div id="content-txt">В&nbsp;соответствии с&nbsp;Положением 
-					о&nbsp;формировании колористического решения фасадов зданий, строений, сооружений в&nbsp;городе Туле <b> (утверждено постановлением администрации города Тулы от&nbsp;09.12.2013 &#8470;&nbsp;4145 
+			<div id="content-txt">В&nbsp;соответствии с&nbsp;Положением
+					о&nbsp;формировании колористического решения фасадов зданий, строений, сооружений в&nbsp;городе Туле <b> (утверждено постановлением администрации города Тулы от&nbsp;09.12.2013 &#8470;&nbsp;4145
 				&laquo;О&nbsp;колористических решениях фасадов зданий, строений, сооружений в&nbsp;городе Туле&raquo;)</b>, каждому зданию в&nbsp;Туле <b>необходимо оформить Паспорт колористического решения фасадов.</b>
 			</div>
 
 		<a class="various order-button" href="#form-ite">ОФОРМИТЬ<br>ЗАЯВКУ</a>
-		
+
 		<div class="clock-ite"></div>
-	
+
 	</main><!-- .content -->
-	
+
 	<div id="content-2">
 		<div id="content-2-txt" >
 					<div id="content-2-h">ОФОРМЛЕНИЕ ПАСПОРТА РЕКЛАМНОЙ КОНСТРУКЦИИ</div>
@@ -61,15 +61,15 @@ $(document).ready(function() {
 		</div>
 			<a class="various order-button" style="left:34px; top:160px;" href="#form-ite">ОФОРМИТЬ<br>ЗАЯВКУ</a>
 	</div>
-	
+
 	<div class="clock-2-ite"></div>
-	
+
 	<div class="days20"> <span>Срок</span> оформления колористического паспорта<br>и паспорта рекламной конструкции <span>20 дней</span><br>с момента подписания договора</div>
-	
+
 	<div class="footer">
 		<div id="footer-txt">Документы, необходимые для оформления</div>
 	</div>
-	
+
 	<div id="footer-ul">
 	<p><b>Паспорта колористического решения фасадов зданий, строений, сооружений:</b></p>
 	<ul>
@@ -77,44 +77,44 @@ $(document).ready(function() {
 		<li>Копия выписки из единого государственного реестра (юридических лиц / индивидуальных предпринимателей).</li>
 	</ul>
 
-	
+
 	<p><b>Паспорта рекламного места (вывески):</b></p>
 	<ul>
 		<li>Адрес размещения, тип и габаритные размеры рекламной конструкции.</li>
 		<li>Копии документов о собственнике рекламной конструкции.</li>
 	</ul>
 
-	
+
 	<p><b>Для юридических лиц:</b></p>
 	<ul>
 		<li>Заверенная заявителем копия устава.</li>
 		<li>Заверенная заявителем копия свидетельства о внесении заявителя в Единый государственный реестр юридических лиц (ЕГРЮЛ).</li>
 		<li>Заверенная заявителем копия свидетельства о присвоении ИНН.</li>
 		<li>Заверенная заявителем копия приказа о назначении директора на должность.</li>
-		
+
 	</ul>
 
-	
+
 	<p><b>Для индивидуальных предпринимателей:</b></p>
 	<ul>
 		<li>Заверенная заявителем копия паспорта.</li>
 		<li>Заверенная заявителем копия свидетельства о внесении в Единый Государственный реестр индивидуальных предпринимателей.</li>
 	</ul>
 
-	
+
 	</div>
-	
+
 	<div id="footer-gray">
 	Для получения подробной консультации звоните:
 	<p style="text-shadow: 2px 2px 2px #231f20;">8 920 751-80-00</p>
 	<a class="various" href="#form-ite">Или оставляйте заявку на обратный звонок</a>
-	
+
 	</div>
-	
-	<div id="footer-white">Мы возьмем на себя все вопросы, связанные с оформлением законности вашей наружной рекламы. 
-Заключив с нами договор на оказание услуг, вы можете быть уверены в полной законности размещемой вами рекламной 
+
+	<div id="footer-white">Мы возьмем на себя все вопросы, связанные с оформлением законности вашей наружной рекламы.
+Заключив с нами договор на оказание услуг, вы можете быть уверены в полной законности размещемой вами рекламной
 конструкции, вывески, объекта.</div>
-	
+
 	<div id="footer-blue">
 		<table>
 			<tr>
@@ -122,11 +122,11 @@ $(document).ready(function() {
 				&copy; <? echo date("Y"); ?> tulagree.ru
 			</td>
 			<td>Вопросы и преложения направляйте по телефону +7 (920) 751-80-00<br>и электронной почте <a style="color:#fff"  href="mailto:zakaz@tulagree.ru">zakaz@tulagree.ru</a></td>
-			
+
 			<td style="padding-left:279px;">
 				Сделано в <a style="color:#fff" href="http://www.it-escort.ru">АйТи-Эскорт</a>
 			</td>
-			
+
 			</tr>
       </table>
 	</div>
@@ -137,23 +137,23 @@ $(document).ready(function() {
 	<div id="form-header">
 		Заявка на паспорт рекламной конструкции
 	</div>
-	
+
 	<form method="post" action="" id="ajaxform">
 		<label class="form-label">Ваше имя</label>
 		<input class="form-input" type="text" id="yourname" name="yourname">
-		
+
 		<label class="form-label">Ваш номер телефона</label>
 		<input class="form-input" type="text" id="yourphone" name="yourphone">
-		
+
 		<label class="form-label">Адрес объекта, для оформления</label>
 		<input class="form-input" type="text" id="youraddress" name="youraddress">
-		
+
 		<label class="form-label">Тип объекта, для оформления</label>
 		<input class="form-input" type="text" id="yourtype" name="yourtype">
-      
-		<div class="form-button" onclick="javascript:submit_form();">ОТПРАВИТЬ</div>		
+
+		<div class="form-button" onclick="javascript:submit_form();">ОТПРАВИТЬ</div>
 	</form>
-	
+
 </div>
 
 <!-- Yandex.Metrika counter -->
